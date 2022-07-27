a17c23b94006c38e33f738081046844db67986bf
Harshit bargujar <harshit.bargujar@rapido.bike>
2022-07-27 08:26:50 +0530

    <Task-001.4> Harshit, Added inches to yard conversion

--- ConversionDevice/src/ConversionDevice.java
+++ ConversionDevice/src/ConversionDevice.java
@@ -9,16 +9,19 @@ class ConversionDevice {
 		System.out.println("Welcome to Conversion Device!!");
 		Scanner scanner = new Scanner(System.in);
 		Integer inputChoice = 0;
+		float inchesToBeConverted, convertedYards, yardsToBeConverted, convertedInches, convertedFeet,
+				feetToBeConverted;
 		do {
-			System.out.println("\n1.Convert Feet to Inches\n2.Convert Yards to Feet\n3.Exit!");
+			System.out
+					.println("\n1.Convert Feet to Inches\n2.Convert Yards to Feet\n3.Convert Inches to Yards\n4.Exit!");
 			inputChoice = scanner.nextInt();
 
 			switch (inputChoice) {
 				case 1:
 					System.out.println("Enter the number in feet : ");
 
-					Float feetToBeConverted = scanner.nextFloat();
-					Float convertedInches = Converter.convertFeetToInches(feetToBeConverted);
+					feetToBeConverted = scanner.nextFloat();
+					convertedInches = Converter.convertFeetToInches(feetToBeConverted);
 
 					System.out.println(feetToBeConverted + " feet in inches = " + convertedInches + " inches");
 					break;
@@ -26,20 +29,37 @@ class ConversionDevice {
 				case 2:
 					System.out.println("Enter the number in yards : ");
 
-					Float yardsToBeConverted = scanner.nextFloat();
-					Float convertedFeet = Converter.convertYardsToFeet(yardsToBeConverted);
+					yardsToBeConverted = scanner.nextFloat();
+					convertedFeet = Converter.convertYardsToFeet(yardsToBeConverted);
 
 					System.out.println(yardsToBeConverted + " yards in feet = " + convertedFeet + " feet");
 					break;
 
 				case 3:
+					System.out.println("Enter the number in inches : ");
+
+					inchesToBeConverted = scanner.nextFloat();
+					convertedYards = Converter.convertInchesToYard(inchesToBeConverted);
+
+					System.out.println(inchesToBeConverted + " inches in yards = " + convertedYards + " yards");
+					break;
+				// case 4:
+				// System.out.println("Enter the number in yards : ");
+
+				// yardsToBeConverted = scanner.nextFloat();
+				// convertedInches = Converter.convertFeetToInches(yardsToBeConverted);
+
+				// System.out.println(yardsToBeConverted + " inches in yards = " +
+				// convertedInches + " yards");
+				// break;
+				case 4:
 					System.out.println("Thanks for using our services!");
 					break;
 
 				default:
 					System.out.println("Please enter a valid choice");
 			}
-		} while (inputChoice != 3);
+		} while (inputChoice != 4);
 		scanner.close();
 	}
 }
--- ConversionDevice/src/logic/Converter.java
+++ ConversionDevice/src/logic/Converter.java
@@ -3,6 +3,8 @@ package ConversionDevice.src.logic;
 public class Converter {
 	private static final Integer FEET_TO_INCH = 12;
 	private static final Integer YARD_TO_FEET = 3;
+	private static final float INCH_TO_FEET = (float) (1.0 / 12.0);
+	private static final float FEET_TO_YARD = (float) (1.0 / 3.0);
 
 	public static Float convertFeetToInches(Float feetToBeConverted) {
 		return feetToBeConverted * FEET_TO_INCH;
@@ -10,5 +12,13 @@ public class Converter {
 
 	public static Float convertYardsToFeet(Float yardsToBeConverted) {
 		return yardsToBeConverted * YARD_TO_FEET;
+	}
+
+	public static float convertInchesToYard(float inchesToBeConverted) {
+		return convertInchesToFeet(inchesToBeConverted) * FEET_TO_YARD;
+	}
+
+	private static float convertInchesToFeet(float inchesToBeConverted) {
+		return inchesToBeConverted * INCH_TO_FEET;
 	}
 }
